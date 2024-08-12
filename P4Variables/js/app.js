1f136c045ae31a45a60f5327eb1dcadb9ed48c4f
jkraak <tom.berglund@gmail.com>
2024-08-12 23:37:48 +0200

    Added Variables

---
+++ P4Variables/js/app.js
@@ -0,0 +1,5 @@
+let NumberOfBottles = 10
+const line1 = "Green Bottled Hanging on the Wall"
+const line2 = "and if One Green Bottle Should Accidentally Fall, There Will Be"
+
+console.log(NumberOfBottle)
