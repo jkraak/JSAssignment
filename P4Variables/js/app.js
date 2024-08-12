ab0f05d987bb342669ecf33f515ac6a1316b8751
jkraak <tom.berglund@gmail.com>
2024-08-12 23:38:19 +0200

    added console log 1

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -3,3 +3,5 @@ const line1 = "Green Bottled Hanging on the Wall"
 const line2 = "and if One Green Bottle Should Accidentally Fall, There Will Be"
 
 console.log(NumberOfBottle)
+console.log (line1)
+
