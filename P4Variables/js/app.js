dff3fe9fe61c9f2177c957b0afc050dc2e08e72a
jkraak <tom.berglund@gmail.com>
2024-08-12 23:45:43 +0200

    Changed letter to uppercase

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -1,10 +1,10 @@
 let numberOfBottles= 10;
 const line1 = "green bottles hanging on the wall";
-const line2 = "and if one green bottle should accidentally fall, there will be";
+const line2 = "And if one green bottle should accidentally fall, there will be";
 
 console.log(numberOfBottles);
 console.log (line1);
 console.log(numberOfBottles);
 console.log(line1);
-console.(line2);
+console.log(line2);
 
