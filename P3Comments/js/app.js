7f702e3d100eb7c8f531fcd0409797b2e9c15fd6
jkraak <tom.berglund@gmail.com>
2024-08-12 23:22:49 +0200

    Created new file plus added text and comments

---
+++ P3Comments/js/app.js
@@ -0,0 +1,11 @@
+/* Author of File: Jenny
+Today's Date: 2024-08-12
+Email Address: tom.berglund@gmail.com
+*/
+
+console.log("Hello World!")
+console.log("Hello World!")
+console.log("Hello World!")
+console.log("Hello World!")
+console.log("Hello World!")
+
