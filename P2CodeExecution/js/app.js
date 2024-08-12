82d4925cd58b9d097c70cdf482d4a6b90e7f9f28
jkraak <tom.berglund@gmail.com>
2024-08-12 20:26:17 +0200

    added lines

---
+++ P2CodeExecution/js/app.js
@@ -0,0 +1,6 @@
+console.log("This is line 1.");
+console.log("This is line 2.");
+console.log("This is line 3."); console.log("This is line 4.");
+console.log("This is line 5.");;
+;
+console.log("This is line 6.");
