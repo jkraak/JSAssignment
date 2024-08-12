81111610b442dea5f2e9a036fd9319001f74004b
jkraak <tom.berglund@gmail.com>
2024-08-12 23:47:33 +0200

    Added new line 3 variable

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -1,10 +1,13 @@
 let numberOfBottles= 10;
 const line1 = "green bottles hanging on the wall";
 const line2 = "And if one green bottle should accidentally fall, there will be";
-
+let line3=numberOfBottles -1;
 console.log(numberOfBottles);
 console.log (line1);
 console.log(numberOfBottles);
 console.log(line1);
 console.log(line2);
+console.log(line3);
+console.log(line1);
+
 
