2fb0b18c0c0a68d144b3650e93fbab98e77eb3e6
jkraak <tom.berglund@gmail.com>
2024-08-12 23:44:42 +0200

    added lines

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -4,4 +4,7 @@ const line2 = "and if one green bottle should accidentally fall, there will be";
 
 console.log(numberOfBottles);
 console.log (line1);
+console.log(numberOfBottles);
+console.log(line1);
+
 
