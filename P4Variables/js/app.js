9239c10c99fc1a47bcdf5b8851e0b643c0e164b4
jkraak <tom.berglund@gmail.com>
2024-08-12 23:43:56 +0200

    Changed cases

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -1,7 +1,7 @@
-let NumberOfBottles = 10
-const line1 = "Green Bottled Hanging on the Wall"
-const line2 = "and if One Green Bottle Should Accidentally Fall, There Will Be"
+let numberOfBottles= 10;
+const line1 = "green bottles hanging on the wall";
+const line2 = "and if one green bottle should accidentally fall, there will be";
 
-console.log(NumberOfBottle)
-console.log (line1)
+console.log(numberOfBottles);
+console.log (line1);
 
