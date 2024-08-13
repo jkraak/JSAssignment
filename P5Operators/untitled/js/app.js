70c730f5d30e5a47842706171d204693458a4a67
jkraak <tom.berglund@gmail.com>
2024-08-13 20:47:21 +0200

    added formulas

---
+++ P5Operators/untitled/js/app.js
@@ -0,0 +1,19 @@
+let speedInKmH = 72;
+let speedInMpH = speedInKmH * 1000;
+let speedinMpS= speedInMpH / 3600;
+console.log(speedinMpS);
+
+
+let numberOfMinutes = 3;
+let numberOfSeconds = numberOfMinutes * 60;
+console.log(numberOfSeconds);
+
+let x = 11;
+let y = 4;
+let z=x / y;
+
+console.log(z)
+
+let d = x % y;
+console.log(d);
+
