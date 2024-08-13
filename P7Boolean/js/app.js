8af829da658803e2a494071ea4485a6c3cc75fc3
jkraak <tom.berglund@gmail.com>
2024-08-13 23:44:27 +0200

    added components

---
+++ P7Boolean/js/app.js
@@ -0,0 +1,10 @@
+let birthYear= 2035;
+let age= 2024 - birthYear;
+let isChild = 0 <= age && age <= 10;
+let isTeenager = 11 <= age && age <= 17;
+let isAdult = age >= 18;
+
+console.log(isChild);
+console.log(isTeenager);
+console.log(isAdult);
+
