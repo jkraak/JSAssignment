ed492b0b524eae9992d74f15616f64ab76529dc4
jkraak <tom.berglund@gmail.com>
2024-08-13 23:07:58 +0200

    Refactored the code

--- P6Types/js/app.js
+++ P6Types/js/app.js
@@ -2,54 +2,15 @@ let numberOfBottles= 10;
 const line1 = "green bottles hanging on the wall";
 const line2 = "And if one green bottle should accidentally fall, there will be";
 let line3=numberOfBottles -1;
-console.log(numberOfBottles);
-console.log (line1);
-console.log(numberOfBottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberOfBottles);
-console.log(line1);
-
-console.log(numberOfBottles);
-console.log (line1);
-console.log(numberOfBottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberOfBottles);
-console.log(line1);
-
-console.log(numberOfBottles);
-console.log (line1);
-console.log(numberOfBottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberOfBottles);
-console.log(line1);
-
-console.log(numberOfBottles);
-console.log (line1);
-console.log(numberOfBottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberOfBottles);
-console.log(line1);
-
-console.log(numberOfBottles);
-console.log (line1);
-console.log(numberOfBottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberOfBottles);
-console.log(line1);
-
-console.log(numberOfBottles);
-console.log (line1);
-console.log(numberOfBottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberOfBottles);
-console.log(line1);
+// console.log(numberOfBottles);
+// console.log (line1);
+// console.log(numberOfBottles);
+// console.log(line1);
+// console.log(line2);
+// console.log(--numberOfBottles);
+// console.log(line1);
 
 
+console.log(numberOfBottles, line1, numberOfBottles, line1, line2, line3, line1);
 
 
