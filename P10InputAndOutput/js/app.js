bfe8bf05e93268c7a77b1c6f0bd956bb31aa6894
jkraak <tom.berglund@gmail.com>
2024-08-16 00:06:20 +0200

    added exercises

---
+++ P10InputAndOutput/js/app.js
@@ -0,0 +1,20 @@
+const numberToSquare= 4
+function squareNumber(numberToSquare) {
+  return numberToSquare * numberToSquare
+}
+
+console.log(squareNumber(numberToSquare))
+
+function average (number1,number2) {
+  return (number1 + number2) /2
+}
+
+console.log (average(2,6));
+
+
+function greeting(firstName, SurName) {
+  return "Welcome " + firstName+ " " + SurName + "!"
+}
+const firstName = "Jenny";
+const surName = "Berglund"
+console.log(greeting(firstName, surName));
