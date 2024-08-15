6ce14beecd24d381fd2c25cd985df50acf04a514
jkraak <tom.berglund@gmail.com>
2024-08-15 21:38:58 +0200

    started new project

---
+++ P8Conditional Branching/js/app.js
@@ -0,0 +1,19 @@
+let birthYear= 2035;
+let favouriteNumber= 21;
+let age= 2024 - birthYear;
+
+let isChild = 0 <= age && age <= 10;
+let isTeenager = 11 <= age && age <= 17;
+let isAdult = age >= 18;
+
+IF (isChild) {console.log("Child");}
+else if (isTeenager) {console.log("Teenager");}
+else {console.log("Adult");}
+
+
+
+
+
+
+
+
