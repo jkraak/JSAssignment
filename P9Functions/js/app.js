aeac1a3b8b9d052f09f05903040a33518c0cc8a8
jkraak <tom.berglund@gmail.com>
2024-08-15 23:17:36 +0200

    added played movements

---
+++ P9Functions/js/app.js
@@ -0,0 +1,50 @@
+// function freakOut () {
+//   console.log("Rage!");
+//   console.log("Attempt to count to ten.");
+//   console.log("Assume everyone has gone mental.");
+//   console.log("Curse the gods, new and old.");
+//   console.log("Try to self-medicate with flagons of ale.");
+//   console.log("Attempt to pick a fight with your own shadow.");
+//   console.log("Fail miserably.");
+//   console.log("Cry.");
+//   console.log("Rinse and repeat.");
+//   console.log("Eventually stumble across a stray cat and discover there is good in this world.");
+// }
+//
+// freakOut();
+// freakOut();
+
+
+function moveForward () {
+  console.log("Move Forward.");
+}
+function turnRight () {
+  console.log("Turn Right.");
+}
+function turnLeft () {
+  turnRight();
+  turnRight();
+  turnRight();
+}
+
+function turnAround () {
+  turnRight();
+  turnRight();
+}
+
+
+moveForward();
+moveForward();
+moveForward();
+turnAround();
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+moveForward();
+turnAround();
+moveForward();
