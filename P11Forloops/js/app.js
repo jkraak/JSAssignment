f53628edf1668135df972aec0c162b5b6b5e9b93
jkraak <tom.berglund@gmail.com>
2024-08-17 01:13:00 +0200

    Did two questions

---
+++ P11Forloops/js/app.js
@@ -0,0 +1,10 @@
+// for (let i = 0; i < 13; i++) {
+//   console.log (Math.floor(Math.random() * 5) + 1);
+// }
+
+// console.log(randomNumber)
+
+
+for (let i = 1; i <= 1024; i *=2) {
+  console.log(i);
+}
